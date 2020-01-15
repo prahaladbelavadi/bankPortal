a6643a94506e2d12a75947000de8a2909064cbff
Prahalad Belavadi <github@belavadi.com>
2020-01-15 19:39:47 +0530

    testing out the filterTable stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,30 @@ import { DataService } from './../data.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  textFilter: string;
+  selectedCity;
   banks: any;
   dataLoadedState = false;
+  filteredBanks :any;
+
+
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredBanks = this.listFilter ? this.doFilter(this.listFilter) : this.banks;
+
+  }
+
+  cities = [
+    { name: 'MUMBAI' },
+    { name: 'BANGALORE' },
+    { name: 'KOCHI' },
+    { name: 'DELHI' },
+    { name: 'ANGOLA' }
+  ];
 
   constructor(private dataSvc: DataService) {
     this.dataSvc.fetchData('mumbai').subscribe(res => {
@@ -17,10 +38,18 @@ export class HomeComponent implements OnInit {
       this.banks = res;
       console.log(res);
     });
+    this.filteredBanks = this.banks;
+    this.listFilter = '';
   }
 
   ngOnInit() {
     this.dataLoadedState = true;
   }
+
+  doFilter(filterBy: string) {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.banks.filter((bank) =>
+        bank.bank_name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
 
 }
