20e38b3bfd429b9cf6220e7bb51db8421bfbf2ed
Prahalad Belavadi <github@belavadi.com>
2020-01-15 20:14:26 +0530

    fetch all city data at once
    Signed-off-by: Prahalad Belavadi <github@belavadi.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,45 +11,19 @@ export class HomeComponent implements OnInit {
   selectedCity;
   banks: any;
   dataLoadedState = false;
-  filteredBanks :any;
 
 
-  _listFilter = '';
-  get listFilter(): string {
-    return this._listFilter;
-  }
-  set listFilter(value: string) {
-    this._listFilter = value;
-    this.filteredBanks = this.listFilter ? this.doFilter(this.listFilter) : this.banks;
-
-  }
-
-  cities = [
-    { name: 'MUMBAI' },
-    { name: 'BANGALORE' },
-    { name: 'KOCHI' },
-    { name: 'DELHI' },
-    { name: 'ANGOLA' }
-  ];
-
-  constructor(private dataSvc: DataService) {
-    this.dataSvc.fetchData('mumbai').subscribe(res => {
-      this.dataSvc.mumbaiData = res;
-      this.banks = res;
-      console.log(res);
+  constructor(public dataSvc: DataService) {
+    this.dataSvc.cities.forEach(city => {
+      this.dataSvc.fetchData(city).subscribe(res => {
+        this.dataSvc.cityBankData[city] = res;
+      });
     });
-    this.filteredBanks = this.banks;
-    this.listFilter = '';
+    console.log(this.dataSvc.cityBankData);
   }
 
   ngOnInit() {
     this.dataLoadedState = true;
   }
-
-  doFilter(filterBy: string) {
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.banks.filter((bank) =>
-        bank.bank_name.toLocaleLowerCase().indexOf(filterBy) !== -1);
-}
 
 }
